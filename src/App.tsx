7e75b29a518f4f79698291927b3d6e8c285f3397
Ali Pouryousefi <a.pouryousefi98@gmail.com>
2025-04-05 09:34:00 +0330

    create project

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const App = () => {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container>
+        <h1>Dark mode</h1>
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default App;
